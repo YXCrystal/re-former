c8082a7b80628f3dd5c055d092c4c5645f6760be
Crystal <yxcwuu@gmail.com>
2020-02-22 13:41:21 -0600

    Fix validation typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     validates :username, presence: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX } 
-    validates :password, presence: true, length: { minumum: 6 } 
+    validates :password, presence: true, length: { minimum: 6 } 
 
-    has_secured_password
+    has_secure_password
 end
