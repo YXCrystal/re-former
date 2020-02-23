5e5ac43715c451c671d6034dab0263fd2808618b
Crystal <yxcwuu@gmail.com>
2020-02-23 14:23:49 -0600

    Add Session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
   root "users#new"
-
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#delete"
   resources :users
 
 end
